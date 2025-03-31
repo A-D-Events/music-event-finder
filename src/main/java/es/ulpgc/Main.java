0fe9b5d1c6b4e1e121f34ad3e387f96f06aae253
Guedess <albertoguedes4@gmail.com>
2025-03-31 13:42:41 +0100

    V0.1.1 - Updated the configuration file.

--- src/main/java/es/ulpgc/Main.java
+++ src/main/java/es/ulpgc/Main.java
@@ -1,9 +1,12 @@
 package es.ulpgc;
 
+import java.io.IOException;
+
 import es.ulpgc.database.DatabaseManager;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         DatabaseManager.initializeDatabase();
         System.out.println("Aplicación iniciada correctamente.");
     }
