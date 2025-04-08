193c09d27d28673ae009513d2ce2fe4039e871d2
Guedes <albertoguedes4@gmail.com>
2025-04-08 17:23:54 +0100

    V0.3.4 - Fixed typo in init_db.sql

--- spotify-feeder/target/classes/init_db.sql
+++ spotify-feeder/target/classes/init_db.sql
@@ -1,6 +1,4 @@
 DROP TABLE IF EXISTS artists;
-DROP TABLE IF EXISTS events;
-DROP TABLE IF EXISTS artist_event;
 
 CREATE TABLE artists (
     id TEXT PRIMARY KEY,
@@ -8,22 +6,4 @@ CREATE TABLE artists (
     popularity INTEGER,
     genre TEXT,
     followers INTEGER
-);
-
-CREATE TABLE events (
-    id TEXT PRIMARY KEY,
-    name TEXT NOT NULL,       
-    date TEXT,                 
-    venue TEXT,              
-    city TEXT,                 
-    country TEXT,              
-    url TEXT 
-);
-
-CREATE TABLE artist_event (
-    artist_id TEXT,
-    event_id TEXT,
-    PRIMARY KEY (artist_id, event_id),
-    FOREIGN KEY (artist_id) REFERENCES artists(id),
-    FOREIGN KEY (event_id) REFERENCES events(id)
 );
